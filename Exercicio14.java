7f930fa42275ad953b01170f1bf3e98ddc78e937
vts1978 <vinicioussouza@gmail.com>
2024-03-14 12:35:40 -0300

    14

---
+++ Exercicio14.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Exercicio14 {
+    public static void Executar() {
+        Scanner leitor = new Scanner(System.in);
+
+        var a1 = 10;
+        var n = 7;
+        var r = 3;
+
+        var result = 0;
+
+        result = a1 + (n - 1) * r;
+
+        System.out.println(result);
+        
+    }
+}
